59ff0e17598ef3d7a3dab09d2d00cbacc29afc99
seghost <deimos12399@gmail.com>
2022-07-11 14:58:09 -0300

    maqueta

---
+++ App.js
@@ -0,0 +1,7 @@
+"use strict";
+var message = "Hello World";
+console.log(message);
+console.log("test");
+console.log("test2");
+console.log("test3");
+console.log("test4");
---
+++ App.ts
@@ -0,0 +1,6 @@
+var message = "Hello World";
+console.log(message);
+console.log("test");
+console.log("test2");
+console.log("test3");
+console.log("test4");
